0594d523a07ec3974cc90483d88fe03ae4c93a72
buckthebot <buckthebot@gmail.com>
2020-01-20 23:59:36 +0100

    Show balance in hello world message

--- check_balance.js
+++ check_balance.js
@@ -1,19 +1,25 @@
 var request = require("request");
 
-request({
-    method: "POST",
-    url: "https://demo-api.ig.com/gateway/deal/session",
-    json: true,
-    body: {
-        identifier: process.argv[2],
-        password: process.argv[3]
-    },
-    headers: {
-        "X-IG-API-KEY": process.argv[4],
-        "Version": 2
-    }
-}, function(error, response, body) {
-    if (response && response.statusCode === 200) {
-        console.log("Your current balance is:", "£" + body.accountInfo.balance);
-    }
-});
+module.exports = function(options, callback) {
+    request({
+        method: "POST",
+        url: "https://demo-api.ig.com/gateway/deal/session",
+        json: true,
+        body: {
+            identifier: options.identifier,
+            password: options.password
+        },
+        headers: {
+            "X-IG-API-KEY": options.apiKey,
+            "Version": 2
+        }
+    }, function(error, response, body) {
+        if (response && response.statusCode === 200) {
+            callback(parseFloat(body.accountInfo.balance));
+        } else {
+            console.error(error, response.statusCode, body);
+            callback(undefined);
+        }
+    });
+};
+
--- index.js
+++ index.js
@@ -1,7 +1,18 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 3000
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+const options = {
+    identifier: process.env.IG_IDENTIFIER || process.argv[2],
+    password: process.env.IG_PASSWORD || process.argv[3],
+    apiKey: process.env.IG_API_KEY || process.argv[4]
+};
+const checkBalance = require("./check_balance");
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => {
+    checkBalance(options, (balance) => {
+        const balanceText = balance === undefined ? "No sure if I have any money." : ("I have £" + balance);
+        res.send('Hello World!' + balanceText);
+    });
+});
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
